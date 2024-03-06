0e4be10be90e714f48be7c07b68df17a9f38c63e
temirovuz <temirov-muhammad@mail.ru>
2024-03-06 15:39:50 +0500

    update views

--- account/views.py
+++ account/views.py
@@ -2,3 +2,19 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+from account.models import User
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 != password2:
+            return HttpResponse('Passwords do not match')
+        else:
+            phone = request.POST['phone']
+            my_user = User.objects.create_user(username=username, email=email, password=password1, phone_numer=phone)
+            my_user.save()
+            
