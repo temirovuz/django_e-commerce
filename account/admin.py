a6edb612870b61cb1ddfb49769c52e4a109db227
temirovuz <temirov-muhammad@mail.ru>
2024-03-06 10:39:27 +0500

    update account app

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from account.models import User, Company, Address, Seller
+
+admin.site.register([User, Company, Address, Seller])
--- account/models.py
+++ account/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from base.models import TimeStampedModel
+
+
+class User(AbstractUser, TimeStampedModel):
+    TYPE_USERS = (
+        (1, 'SuperAdmin'),
+        (2, 'Admin'),
+        (3, 'Customer'),
+        (4, 'Salesman')
+    )
+
+    user_type = models.PositiveSmallIntegerField(choices=TYPE_USERS, default=3)
+    phone_number = models.CharField(max_length=13, unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Company(TimeStampedModel):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(TimeStampedModel):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class Seller(TimeStampedModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='seller', null=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='seller', null=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='seller', null=True)
+
+    def __str__(self):
+        return self.user.username
+
